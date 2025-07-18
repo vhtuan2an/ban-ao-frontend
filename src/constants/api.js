db9b2e834afdd314b335717bc821faef827cac80
Võ Hoàng Tuấn <166472565+vhtuan2an@users.noreply.github.com>
2025-07-18 13:06:12 +0700

    fix: Update API base URL to use production endpoint

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,8 @@
 // src/api/api.js
 import axios from 'axios';
 
-// const BASE_URL = 'https://ban-ao-backend.onrender.com/'; 
-const BASE_URL = 'http://localhost:3000/'; 
+const BASE_URL = 'https://ban-ao-backend.onrender.com/'; 
+// const BASE_URL = 'http://localhost:3000/'; 
 
 const api = axios.create({
   baseURL: BASE_URL,
